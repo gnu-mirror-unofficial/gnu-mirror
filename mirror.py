c4d1fb1de1e60684715904ff18917d8f539df475
JMcB <joel.mcbride1@live.com>
2021-11-20 10:58:27 +0000

    Bugfix

--- mirror.py
+++ mirror.py
@@ -5,6 +5,7 @@ import re
 import subprocess
 import concurrent.futures
 from pathlib import Path
+from typing import Union
 
 import requests
 from bs4 import BeautifulSoup
@@ -18,12 +19,13 @@ SAVANNAH_SEARCH_FORMAT = 'https://savannah.gnu.org/search/?type_of_search=soft&w
 SAVANNAH_SEARCH_ROWS = 1000
 SAVANNAH_PROJECT_FORMAT = 'https://savannah.gnu.org/projects/{}'
 SAVANNAH_GIT_FORMAT = 'https://git.savannah.gnu.org/git/{}.git'
-MIRROR_GITHUB_ORG = 'git-mirror-unofficial'
+MIRROR_GITHUB_ORG = 'gnu-mirror-unofficial'
 MIRROR_GIT_FORMAT = f'https://github.com/{MIRROR_GITHUB_ORG}/{{}}'
 GNU_PROJECT_REGEX = re.compile(r'\.\./projects/(.*)')
-MIRROR_DESCRIPTION_FORMAT = f"""\
-{SAVANNAH_PROJECT_FORMAT}
-Please read {MIRROR_GIT_FORMAT.format('gnu-mirror#readme')}
+# not even newlines are allowed in github repo descs lol
+MIRROR_DESCRIPTION_FORMAT = """\
+Official repo link below. \
+Please read this organisation's pinned readme for info.\
 """
 
 
@@ -43,9 +45,11 @@ def get_all_projects() -> list[str]:
     return project_names
 
 
-def run_git_command(directory: Path, command: str):
-    command_list = command.split()
-    subprocess.run([GIT, '-C', str(directory), *command_list])
+def run_git_command(directory: Path, command: Union[str, list]):
+    if isinstance(command, str):
+        command = command.split()
+    print(command)
+    subprocess.run([GIT, '-C', str(directory), *command])
 
 
 # https://cli.github.com/manual/
@@ -57,6 +61,7 @@ def get_existing_repos(owner: str = MIRROR_GITHUB_ORG) -> list[str]:
 
 
 def sync_project(project: str, workdir: Path, mirror_exists: bool = False):
+    print(f'Mirroring project {project}.')
     # todo: remove once done testing
     input(f'Press enter to sync {project}:\n')
 
@@ -66,9 +71,13 @@ def sync_project(project: str, workdir: Path, mirror_exists: bool = False):
     project_link = SAVANNAH_PROJECT_FORMAT.format(project)
 
     if not work_tree.is_dir():
+        print('Local copy does not exist, cloning.')
         run_git_command(workdir, f'clone {origin_remote}')
+    else:
+        print('Local copy already exists.')
     if not mirror_exists:
-        repo_description = MIRROR_DESCRIPTION_FORMAT.format(project)
+        print('Mirror repo does not exist, creating.')
+        repo_description = MIRROR_DESCRIPTION_FORMAT
         subprocess.run(
             [
                 GH, 'repo', 'create',
@@ -78,16 +87,20 @@ def sync_project(project: str, workdir: Path, mirror_exists: bool = False):
                 '--public', '-y',
             ]
         )
+    else:
+        print('Mirror repo already exists.')
 
     run_git_command(work_tree, 'pull')
-    run_git_command(work_tree, f'push {mirror_remote}')
+    run_git_command(work_tree, ['push', mirror_remote])
 
 
 def sync_all_projects(projects: list[str], workdir: Path):
     existing_repos = get_existing_repos()
 
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        executor.map(lambda p: sync_project(p, workdir, mirror_exists=(p in existing_repos)), projects)
+    # with concurrent.futures.ProcessPoolExecutor() as executor:
+    #     executor.map(lambda p: sync_project(p, workdir, mirror_exists=(p in existing_repos)), projects)
+    for p in projects:
+        sync_project(p, workdir, mirror_exists=(p in existing_repos))
 
 
 def main():
